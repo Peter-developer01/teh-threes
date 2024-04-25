b4ed940b1db93c5929d8d74f6d78076d5e362085
Peter Kolosov <101456132+Peter-developer01@users.noreply.github.com>
2024-04-25 20:45:34 +0300

    upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const numbers = document.querySelector(".numbers");
+const check = document.querySelector(".check");
+
+// When clicking on the check button, we will brute-force every number and output ALL possible variants with three terms in it.
+check.addEventListener("click", () => {
+	const result = document.querySelector(".result");
+	const arr = numbers.value.split("\n").map((num) => num.trim().replaceAll(" ", "").replaceAll(",", "."));
+	const results = [];
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = 0; j < arr.length; j++) {
+			for (let k = 0; k < arr.length; k++) {
+				results.push(`<li>${arr[i]} + ${arr[j]} + ${arr[k]} = ${Number(arr[i]) + Number(arr[j]) + Number(arr[k])}</li>`);
+			}
+		}
+	}
+
+	result.innerHTML = results.join("\n");
+})
